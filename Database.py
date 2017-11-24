4b4f52ac0a5958f5d9b32b059131e39467737217
Shashank Khare <shashank88@gmail.com>
2017-11-24 23:36:04 +0000

    Move color to constants

--- Database.py
+++ Database.py
@@ -1,21 +1,9 @@
 import sys, re
 import unicodedata
 
+from constants import color
 from Table import Table
 
-
-class color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    ITALIC = '\033[3m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
 
 class Database:
     thesaurus_object = None
--- ln2sql.py
+++ ln2sql.py
@@ -2,24 +2,13 @@
 import os, sys, getopt
 import unicodedata
 
+from constants import color
 from Database import Database
 from LangConfig import LangConfig
 from Parser import Parser
 from Thesaurus import Thesaurus
 from StopwordFilter import StopwordFilter
 
-
-class color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
 
 class ln2sql:
     def __init__(self, database_path, language_path, input_sentence, json_output_path, thesaurus_path, stopwords_path):
