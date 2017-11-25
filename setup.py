36c4401f5f11bc4a5a7f809c3bc25ea014b32276
Shashank Khare <shashank88@gmail.com>
2017-11-25 21:56:56 +0000

    fix email in setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url='https://github.com/FerreroJeremy/ln2sql',
     license='GNU',
     author='Jérémy Ferrero,Shashank Khare',
-    author_email='shashank88@gmail.com',
+    author_email='jeremy.ferrero@compilatio.net,shashank88@gmail.com',
     description='Convert Natural Language to SQL queries',
     packages=find_packages(exclude=['tests']),
     long_description=open('README.md').read(),
