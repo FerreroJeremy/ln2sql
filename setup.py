86b9318723ed815eaf909321e357dc4668434b9f
Shashank Khare <shashank88@gmail.com>
2017-11-25 21:48:50 +0000

    Add setup.py and push to pypi
    It's live on https://pypi.python.org/pypi/ln2sql
    
    Need to add __init__.py and __main__.py to make it more usable

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='ln2sql',
+    version='0.1',
+    url='https://github.com/FerreroJeremy/ln2sql',
+    license='GNU',
+    author='Jérémy Ferrero,Shashank Khare',
+    author_email='shashank88@gmail.com',
+    description='Convert Natural Language to SQL queries',
+    packages=find_packages(exclude=['tests']),
+    long_description=open('README.md').read(),
+    setup_requires=['pytest'],
+) 
