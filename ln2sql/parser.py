f309fa05fb7cd4ff73e4222f70e6d93f1ec36dd9
maverickjoy <vikram.singh@heyzot.com>
2017-12-08 22:56:02 +0530

    #offset buffer fixing

--- ln2sql/parser.py
+++ ln2sql/parser.py
@@ -416,51 +416,63 @@ class WhereParser(Thread):
 
                 for keyword in self.count_keywords:
                     if keyword in phrase_offset_string :    # before the column
-                        self.count_keyword_offset.append(i)
+                        if (phrase_offset_string.find(keyword) + len(keyword) + 1 == len(phrase_offset_string) ) :
+                            self.count_keyword_offset.append(i)
 
                 for keyword in self.sum_keywords:
                     if keyword in phrase_offset_string :    # before the column
-                        self.sum_keyword_offset.append(i)
+                        if (phrase_offset_string.find(keyword) + len(keyword) + 1 == len(phrase_offset_string) ) :
+                            self.sum_keyword_offset.append(i)
 
                 for keyword in self.average_keywords:
                     if keyword in phrase_offset_string :    # before the column
-                        self.average_keyword_offset.append(i)
+                        if (phrase_offset_string.find(keyword) + len(keyword) + 1 == len(phrase_offset_string) ) :
+                            self.average_keyword_offset.append(i)
 
                 for keyword in self.max_keywords:
                     if keyword in phrase_offset_string :    # before the column
-                        self.max_keyword_offset.append(i)
+                        if (phrase_offset_string.find(keyword) + len(keyword) + 1 == len(phrase_offset_string) ) :
+                            self.max_keyword_offset.append(i)
 
                 for keyword in self.min_keywords:
                     if keyword in phrase_offset_string :    # before the column
-                        self.min_keyword_offset.append(i)
+                        if (phrase_offset_string.find(keyword) + len(keyword) + 1 == len(phrase_offset_string) ) :
+                            self.min_keyword_offset.append(i)
 
                 for keyword in self.greater_keywords:
                     if keyword in phrase_offset_string :    # after the column
-                        self.greater_keyword_offset.append(i)
+                        if (phrase_offset_string.find(keyword) + len(keyword) + 1 == len(phrase_offset_string) ) :
+                            self.greater_keyword_offset.append(i)
 
                 for keyword in self.less_keywords:
                     if keyword in phrase_offset_string :    # after the column
-                        self.less_keyword_offset.append(i)
+                        if (phrase_offset_string.find(keyword) + len(keyword) + 1 == len(phrase_offset_string) ) :
+                            self.less_keyword_offset.append(i)
 
                 for keyword in self.between_keywords:
                     if keyword in phrase_offset_string :    # after the column
-                        self.between_keyword_offset.append(i)
+                        if (phrase_offset_string.find(keyword) + len(keyword) + 1 == len(phrase_offset_string) ) :
+                            self.between_keyword_offset.append(i)
 
                 for keyword in self.junction_keywords:
                     if keyword in phrase_offset_string :    # after the column
-                        self.junction_keyword_offset.append(i)
+                        if (phrase_offset_string.find(keyword) + len(keyword) + 1 == len(phrase_offset_string) ) :
+                            self.junction_keyword_offset.append(i)
 
                 for keyword in self.disjunction_keywords:
                     if keyword in phrase_offset_string :    # after the column
-                        self.disjunction_keyword_offset.append(i)
+                        if (phrase_offset_string.find(keyword) + len(keyword) + 1 == len(phrase_offset_string) ) :
+                            self.disjunction_keyword_offset.append(i)
 
                 for keyword in self.negation_keywords:
                     if keyword in phrase_offset_string :
-                        self.negation_keyword_offset.append(i)
+                        if (phrase_offset_string.find(keyword) + len(keyword) + 1 == len(phrase_offset_string) ) :
+                            self.negation_keyword_offset.append(i)
 
                 for keyword in self.like_keywords:
                     if keyword in phrase_offset_string :    # after the column
-                        self.like_keyword_offset.append(i)
+                        if (phrase_offset_string.find(keyword) + len(keyword) + 1 == len(phrase_offset_string) ) :
+                            self.like_keyword_offset.append(i)
 
 
         for table_of_from in self.tables_of_from:
